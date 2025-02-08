68fec0a577588661a80ed0eefbe090066043245b
Nicholas Wise <nicholasmwise@gmail.com>
2025-02-07 21:57:38 -0600

    Set up project files Set up function definition and declarations

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,5 +1,22 @@
+///////////////////////////
 // Nicholas Wise
 // Project #1 - Arrays
+///////////////////////////
 
-using namespace std;
+#include <utils.h>
 
+int main() {
+    cout << "Hello, welcome to N Wise Project 1" << endl;
+
+    // Open Data File, and load data into one dimensional array
+
+    // Modify value of int when called with index of int in the array
+    // return new value and old value back to the user
+
+    // Adds new integer to end of array
+
+    // intake index of array, removes int at index from array
+    return 0;
+}
+
+///////////////////////////
---
+++ Project1/utils.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <utils.h>
+
+// return new value and old value back to the user
+int entry_checker(int num) {
+    return 0;
+}
+
+// Modify value of int when called with index of int in the array
+// return new value and old value back to the user
+tuple<int, int> modify_entry(int num) {
+    return make_tuple(0, 0);
+}
+
+// Adds new integer to end of array
+void add_entry(int num) {
+
+}
+
+// intake index of array, removes int at index from array
+void remove_entry(int num) {
+
+}
+
+
